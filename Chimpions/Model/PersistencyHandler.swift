05cc91e839ea2ca690cf757472a00a75026c2124
Nunzio Ricci <nunzio.ricci.2000@gmail.com>
2023-02-23 17:10:20 +0100

    Added PersistencyHandler.swift and DataModel

---
+++ Chimpions/Model/PersistencyHandler.swift
@@ -0,0 +1,14 @@
+//
+//  PersistencyHandler.swift
+//  Chimpions
+//
+//  Created by Nunzio Ricci on 23/02/23.
+//
+
+import Foundation
+
+class PersistencyHandler {
+    public static let shared = PersistencyHandler()
+    private init() {}
+    
+}
