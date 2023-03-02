53944a2aba3994eac3416f5d602ce22eec33e301
bbwark <96076911+bbwark@users.noreply.github.com>
2023-03-02 16:45:56 +0100

    Update TaskTimerDetail.swift
    TODO: fix those fucking horrible broken swift transition

--- Chimpions/Views/TaskTimerDetail.swift
+++ Chimpions/Views/TaskTimerDetail.swift
@@ -11,10 +11,9 @@ struct TaskTimerDetail: View {
     
     @State var animationStatus: Double = 0
     @State var task: CTask
-    var elapsedRatio: Double {
-        task.elapsedTime / task.duration
-    }
+    @State var elapsedRatio: Double = 0
     @State var timer: Timer?
+    @State var timerFinished = false
     
     var body: some View {
         
@@ -24,8 +23,8 @@ struct TaskTimerDetail: View {
             ZStack{
                 StreamShape(animationStatus: animationStatus)
                     .foregroundColor(.white)
-                    .frame(height: 900)
-                    .offset(y: 600) //TODO: dinamically assignation
+                    .frame(height: 1000)
+                    .offset(y: 600 * abs(elapsedRatio-1))
                     .onAppear(){
                         withAnimation(.linear(duration: 2).repeatForever(autoreverses: false)){
                             animationStatus = 1
@@ -35,7 +34,7 @@ struct TaskTimerDetail: View {
                 Circle()
                     .foregroundColor(.white)
                     .frame(width: 30)
-                    .offset(y: -495) //TODO: animation
+                    .offset(y: -495)
                 
                 VStack{
                     HStack {
@@ -57,28 +56,59 @@ struct TaskTimerDetail: View {
                                 .frame(width: 50)
                                 .offset(x: offset + 55, y: -55)
                         }
-                        
-                        VStack (alignment: .leading){
-                            Text(task.name ?? "Unknown")
-                                .font(.system(size: 24))
-                                .fontWeight(.bold)
-                                .fontDesign(.rounded)
-                            Text(durationConverter(durationInSeconds: Int(task.duration)))
-                                .font(.system(size: 40))
-                                .fontWeight(.bold)
-                                .fontDesign(.rounded)
+                        if !timerFinished{
+                            VStack (alignment: .leading){
+                                Text(task.name ?? "Unknown")
+                                    .font(.system(size: 24))
+                                    .fontWeight(.bold)
+                                    .fontDesign(.rounded)
+                                Text(durationConverter(durationInSeconds: Int(task.duration - task.elapsedTime)))
+                                    .font(.system(size: 40))
+                                    .fontWeight(.bold)
+                                    .fontDesign(.rounded)
+                            }
+                            .offset(x: offset + 40, y: 15)
                         }
-                        .offset(x: offset + 40, y: 15)
                         Spacer()
                     }
                 }
                 .offset(y: 310)
+                
+                if timerFinished {
+                    VStack(spacing: 15){
+                        Text("You spilled")
+                            .font(.system(size: 24))
+                            .fontWeight(.bold)
+                            .fontDesign(.rounded)
+                        Text(durationConverter(durationInSeconds: Int(task.duration)))
+                            .font(.system(size: 60))
+                            .fontWeight(.bold)
+                            .fontDesign(.rounded)
+                        Text("Need more drops?")
+                            .font(.system(size: 24))
+                            .fontWeight(.bold)
+                            .fontDesign(.rounded)
+                    }
+                    .transition(.opacity)
+                }
                 
             }
             
         }.onAppear {
             timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
                 task.elapsedTime += 1
+                withAnimation(.linear(duration: 1)){
+                    if task.elapsedTime <= task.duration{
+                        elapsedRatio = task.elapsedTime / task.duration
+                    }
+                }
+                
+                if task.elapsedTime >= task.duration{
+                    timer.invalidate()
+                    withAnimation(.linear(duration: 0.5)){
+                        timerFinished = true
+                    }
+                }
             }
         }.onDisappear {
             try? PersistencyManager.shared.save(task: task)
@@ -108,7 +138,7 @@ func durationConverter(durationInSeconds: Int) -> String {
             result = String(durationInHours) + "h " + String(minuteOverHour) + "min"
         }
     }
-    if durationInSeconds<60{
+    if durationInSeconds < 60{
         result = "< 1min"
     }
     
@@ -118,6 +148,10 @@ func durationConverter(durationInSeconds: Int) -> String {
 struct TaskTimerDetail_Previews: PreviewProvider {
     static var previews: some View {
         
-        TaskTimerDetail(task: PersistencyManager.preview.getAllTasks()[0])
+        TaskTimerDetail(task: PersistencyManager.preview { manager in
+            let project = CProject(name: "Presentation")
+            try? manager.save(project: project)
+            try? manager.save(task: CTask(projectId: project.id, duration: 5))
+        }.getAllTasks()[0])
     }
 }
