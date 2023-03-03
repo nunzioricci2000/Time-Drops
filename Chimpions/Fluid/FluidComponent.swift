6ca1af23b79b8762140891873a5d36ef8d4ef25e
Nunzio Ricci <nunzio.ricci.2000@gmail.com>
2023-03-03 17:02:56 +0100

    Create FluidComponent

---
+++ Chimpions/Fluid/FluidComponent.swift
@@ -0,0 +1,20 @@
+//
+//  FluidComponent.swift
+//  Chimpions
+//
+//  Created by Nunzio Ricci on 03/03/23.
+//
+
+import SwiftUI
+
+struct FluidComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FluidComponent_Previews: PreviewProvider {
+    static var previews: some View {
+        FluidComponent()
+    }
+}
